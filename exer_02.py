01a51fc8de975e6658e3760df5b27db4fcd18770
IGOR BARBOSA <77306197+igorBR0@users.noreply.github.com>
2024-09-10 23:44:42 -0300

    Add files via upload

---
+++ exer_02.py
@@ -0,0 +1,17 @@
+from time import sleep
+
+
+n0 = 0
+n1 = 1
+r = 0
+flag_number =int(input("Digite um numero, e veremos se ele esta contigo na sequencia de fibonacchi"))
+while(True):
+    r = (n0 + n1)
+    n0 = n1
+    n1 = r
+    print(r) 
+    if r == flag_number:
+        print(f"Numero {flag_number}, pertencia a sequencia de fibonacchi")
+        break
+    #sleep(1)
+        
