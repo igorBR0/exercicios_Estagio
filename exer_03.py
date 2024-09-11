c73afa06f1a62343f89834604b557beaeca3cce7
IGOR BARBOSA <77306197+igorBR0@users.noreply.github.com>
2024-09-11 00:12:53 -0300

    Add files via upload

--- exer_03.py
+++ exer_03.py
@@ -2,18 +2,26 @@ faturamento = [236, 487, 195, 762, 312, 920, 567, 389, 781, 654, 123, 890, 341, 
 
 flag = 0
 max_fat = 0
+min_fat  =  0
+fat_media = 0
 
 for i in range(31):
     if faturamento[i] > max_fat:
         max_fat = faturamento[i]
-min_fat = max_fat
+    fat_media = faturamento[i] + fat_media
 
+
+min_fat = max_fat
+fat_media = fat_media / len(faturamento)
 for j in range(31):
     if min_fat > faturamento[j]:
         min_fat = faturamento[j]
+    if fat_media > faturamento[j]:
+        flag = flag + 1
 
-print (max_fat)
-print(min_fat)
+print(f"Numero de dias que o valor foi maior que a media: {flag }")
+print (f"Maximo {max_fat}")
+print(f"Minimo {min_fat}")
 
    
      
